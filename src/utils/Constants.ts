2586061f06de832561d912964567f29257b4d7fc
Leandro Varela Vargas <lvarela@certant.com>
2022-09-08 22:55:45 -0300

    Creado archivo para constantes útiles.

---
+++ src/utils/Constants.ts
@@ -0,0 +1,16 @@
+import { IConstantsOfErrors } from "../models/IConstantsOfErrors";
+
+export const HEADERS = {
+    HEADER_REQUEST_ID: "Request-Id",
+};
+
+export const API_ERRORS : IConstantsOfErrors = {
+
+    USUARIO_YA_REGISTRADO: {
+        codigo: 1000,
+        mensaje: "Este usuario ya se encuentra registrado."
+    },
+
+    
+
+}
