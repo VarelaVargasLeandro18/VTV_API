633601af55e2e22bfe1b999cf6ce3618cf51ce99
Leandro Varela Vargas <lvarela@certant.com>
2022-09-09 01:35:40 -0300

    Creada validación custom para fechas con Joi

---
+++ src/utils/CustomJoiValidations.ts
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+export const dateJoiCustomValidation = (value : any, helper : Joi.CustomHelpers<any>) => {
+    const [year, month, day] = value.split('/');
+
+    if ( isNaN( Date.parse(new Date(year, month, day).toString()) ) ) {
+        return helper.error("noesfecha");
+    }
+    
+    return value;
+
+}
