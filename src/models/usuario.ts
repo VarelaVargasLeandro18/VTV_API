01281ef7d04ee5be993d99480b4b1f21a13d7a91
Leandro Varela Vargas <lvarela@certant.com>
2022-09-09 01:48:58 -0300

    Terminadas validaciones Joi para usuarios.

--- src/models/usuario.ts
+++ src/models/usuario.ts
@@ -1,23 +1,31 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, DeleteDateColumn } from "typeorm"
 import Joi from "joi";
-import { campoRequerido } from "../utils/mensajesDeValidacion";
+import { campoLargoMaximo, campoLargoMinimo, campoNoEsEmail, campoNoPuedeEstarVacio, campoRequerido } from "../utils/mensajesDeValidacion";
+import { dateJoiCustomValidation } from "../utils/CustomJoiValidations";
 
 export const userRegisterSchema = Joi.object({
 
     email: Joi.string().email().required().messages({
-        'any.required': campoRequerido("email")
+        'any.required': campoRequerido("email"),
+        'string.email': campoNoEsEmail("email"),
     }),
     nombre: Joi.string().required().messages({
-        'any.required': campoRequerido("nombre")
+        'any.required': campoRequerido("nombre"),
+        'string.empty': campoNoPuedeEstarVacio("apellido"),
     }),
     apellido: Joi.string().required().messages({
-        'any.required': campoRequerido("apellido")
+        'any.required': campoRequerido("apellido"),
+        'string.empty': campoNoPuedeEstarVacio("apellido"),
     }),
     contrasenia: Joi.string().min(6).max(20).required().messages({
-        'any.required': campoRequerido("contrasenia")
+        'any.required': campoRequerido("contrasenia"),
+        'string.empty': campoNoPuedeEstarVacio("contrasenia"),
+        'string.min': campoLargoMinimo("contrasenia", 6),
+        "string.max": campoLargoMaximo("contrasenia", 20),
     }),
-    fechaNacimiento: Joi.date().required().messages({
-        'any.required': campoRequerido("fechaNacimiento")
+    fechaNacimiento: Joi.string().required().custom( dateJoiCustomValidation ).messages({
+        'any.required': campoRequerido("fechaNacimiento"),
+        'noesfecha': "No cumple con el formato de fecha."
     }),
     
 }).options({ abortEarly: false });
@@ -54,6 +62,6 @@ export class Usuario {
     fechaEliminado : Date;
 
     @Column("date")
-    fechaNacimiento : Date;
+    fechaNacimiento : string;
 
 }
