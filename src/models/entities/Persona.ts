60027b23c02a2a33f8da902d80d34d6c8de02645
Leandro Varela Vargas <lvarela@certant.com>
2022-09-19 19:48:50 -0300

    DocumentoIdentificador se crea, modifica y elimina a crear a la persona correspondiente.

--- src/models/entities/Persona.ts
+++ src/models/entities/Persona.ts
@@ -29,7 +29,7 @@ export abstract class Persona {
     @PrimaryGeneratedColumn("uuid")
     id : string;
 
-    @OneToOne( () => DocumentoIdentificador, (documento) => documento.persona, {
+    @OneToOne( () => DocumentoIdentificador, {
         cascade: true
     } )
     @JoinColumn()
